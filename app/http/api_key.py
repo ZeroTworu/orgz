5bd8f5d12c4e495c1e39855101802d90ec5d590e
Zero Two <hau.au.999@gmail.com>
2025-08-03 02:30:46 +0500

    Add noqa WPS404

--- app/http/api_key.py
+++ app/http/api_key.py
@@ -3,7 +3,7 @@ from fastapi import Header, HTTPException, status
 from app.settings import ORGZ_API_KEY
 
 
-async def verify_api_key(x_api_key: str = Header(..., alias='X-ORGZ-Key')):
+async def verify_api_key(x_api_key: str = Header(..., alias='X-ORGZ-Key')):  # noqa: WPS404
     if x_api_key != ORGZ_API_KEY:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
